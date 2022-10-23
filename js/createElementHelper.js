230bc1ce6a94975b3d9e7199850fbe27acab5ce7
Smaidziunas <sdamarcius@gmail.com>
2022-10-23 20:43:27 +0300

    namu uzduotis

--- js/createElementHelper.js
+++ js/createElementHelper.js
@@ -16,6 +16,9 @@ pEl.className = 'text';
 pEl.textContent = 'I am text';
 appEl.append(pEl);
 
+
+
+// ============================ FUNCTION TO CREATE ELEMENT ======================
 function crEl(tagName, dest, className, text) {
   const el = document.createElement(tagName);
   if (className) el.className = className;
@@ -27,18 +30,26 @@ function crEl(tagName, dest, className, text) {
 
 crEl('h2', appEl, 'subTitle', 'Hello Friday');
 
+// jeigu norime sukurti li is masyvo(array):
 {
   /* <ul>
   <li></li>
 </ul> */
 }
 
+
+// ===================== funkcija sukurti li viduje sukurto elemento ul: ================
+
+// sukuriam mayva:
 const seasons = ['summer', 'autum', 'winter', 'spring'];
 const colors = ['red', 'green', 'blue'];
 
 function makeList(arrToMakeListFrom, dest) {
+  // sukuriam ul elementa pagal anksciau aprasyta funkcija crEl:
   let ulEl = crEl('ul', dest, 'card');
+  // kuriam loop'sa, kuriame dest argumento vietoje nurodome ul, o teksto vietoje, dedame masyvo arrToMakeListFrom[i];
   for (let item of arrToMakeListFrom) {
+     // crEl argumentai is anksciau: crEl(tagName, dest, className, text)'
     crEl('li', ulEl, '', item);
   }
 }
